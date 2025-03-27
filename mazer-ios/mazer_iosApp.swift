fe38f96ebc063a6b30154b99ef405d9608176083
Jeffrey Isabella <78503766+jmisabella@users.noreply.github.com>
2025-03-26 20:50:49 -0400

    initial blank Swift iOS App project

---
+++ mazer-ios/mazer_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  mazer_iosApp.swift
+//  mazer-ios
+//
+//  Created by Jeffrey Isabella on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct mazer_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
